cd91c59f4027e7fb616b4bc90c56e2df61ddc2b5
Aleksey <rusal@bk.ru>
2023-04-20 13:02:08 +0300

    st struct

--- DB_of_students_m3_23.cpp
+++ DB_of_students_m3_23.cpp
@@ -1,5 +1,31 @@
 ﻿
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+struct StudentNode
+{
+	string surName;
+	string name;
+	string middleName;
+	string faculty;
+	string department;
+	string group;
+	string recordÑardNumber;
+	string birthDateString;
+	bool sex; // true - ìàëü÷èê
+	// false - äåâî÷êà
+	int startYear;
+
+	ExamsRecords examsRecordsData[9][10];
+	StudentNode* next;
+	int id;
+	double avrMarks;
+
+
+};
+
 
 int main()
 {
